f5bfaf71687c3a4dc139384805fb254a68a80afa
Emir Unlu <cenonx@gmail.com>
2021-05-27 23:01:14 +0300

    Add additional params to request

--- crawler/get_reddit.py
+++ crawler/get_reddit.py
@@ -1,16 +1,24 @@
 import json
 import requests
 
-BASE_API_URL = "https://api.pushshift.io"
-SUBMISSION_API_URL = "/reddit/search/submission/"
-COMMENT_API_URL = "/reddit/search/comment/"
-LIMIT_SUBMISSIONS = 100
+BASE_API_URL        = "https://api.pushshift.io"
+SUBMISSION_API_URL  = "/reddit/search/submission/"
+COMMENT_API_URL     = "/reddit/search/comment/"
 
-keywords = [""]
+query               = "test"        # Add your query
+duration            = "30d"         # Select the timeframe. Epoch value or Integer + "s,m,h,d" (i.e. "second", "minute", "hour", "day")
+size                = 1000          # maximum 1000 comments
+sort_type           = "score"       # Sort by score (Accepted: "score", "num_comments", "created_utc")
+sort                = "desc"        # sort descending
+aggs                = "subreddit"   # "author", "link_id", "created_utc", "subreddit"
 
-def search_subs_reddit(query):
-    request = requests.get(BASE_API_URL + SUBMISSION_API_URL + f"?q={query}")
+def search_subs_reddit(**kwargs):
+    request = requests.get(BASE_API_URL + SUBMISSION_API_URL, params = kwargs)
     json_response = request.json()
     return json_response
 
-print(search_subs_reddit("test"))
+print(search_subs_reddit(q=query,                 
+                   after=duration,          
+                   size=size,               
+                   sort_type=sort_type,
+                   sort=sort))
