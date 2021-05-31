d9eaea28fe7c7303f2a49a11e47c1ebaaff0f265
Emir Unlu <cenonx@gmail.com>
2021-05-31 03:29:40 +0300

    Logging and rename

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from analyzer.vader_analyzer import *
 from crawler.get_reddit import *
 from price_tracker.get_price import *
 
-query               = "btc | bitcoin"   # Add your query
-duration            = "7d"             # Select the timeframe. Epoch value or Integer + "s,m,h,d" (i.e. "second", "minute", "hour", "day")
+query               = "btc | bitcoin"    # Add your query
+duration            = "7d"               # Select the timeframe. Epoch value or Integer + "s,m,h,d" (i.e. "second", "minute", "hour", "day")
 size                = 1000               # maximum 1000 comments
-sort_type           = "score"           # Sort by score (Accepted: "score", "num_comments", "created_utc")
-sort                = "desc"            # sort descending
-subs                = ["SatoshiStreetBets"]
+sort_type           = "score"            # Sort by score (Accepted: "score", "num_comments", "created_utc")
+sort                = "desc"             # sort descending
+subs                = ["CryptoMarkets"]
 
 
-comments1 = search_subs_reddit(q=query,
+comments_21d = search_subs_reddit(q=query,
                    before="22d",
                    after="28d",
                    size=size,
@@ -19,7 +19,7 @@ comments1 = search_subs_reddit(q=query,
                    sort=sort,
                    subreddit=subs)
 
-comments2 = search_subs_reddit(q=query,
+comments_14d = search_subs_reddit(q=query,
                    before="15d",
                    after="21d",
                    size=size,
@@ -27,7 +27,7 @@ comments2 = search_subs_reddit(q=query,
                    sort=sort,
                    subreddit=subs)
 
-comments3 = search_subs_reddit(q=query,
+comments_7d = search_subs_reddit(q=query,
                    before="8d",
                    after="14d",
                    size=size,
@@ -35,27 +35,32 @@ comments3 = search_subs_reddit(q=query,
                    sort=sort,
                    subreddit=subs)
 
-date21d = datetime.date.today() - datetime.timedelta(days=21)
-date14d = datetime.date.today() - datetime.timedelta(days=14)
-date7d = datetime.date.today() - datetime.timedelta(days=7)
+date_21d = datetime.date.today() - datetime.timedelta(days=21)
+date_14d = datetime.date.today() - datetime.timedelta(days=14)
+date_7d = datetime.date.today() - datetime.timedelta(days=7)
 
-scores21d = analyze_sentences(comments1)
-scores14d = analyze_sentences(comments2)
-scores7d = analyze_sentences(comments3)
+scores_21d = analyze_sentences(comments_21d)
+scores_14d = analyze_sentences(comments_14d)
+scores_7d = analyze_sentences(comments_7d)
 
-btc_price_21d = get_coingecko_price_history(date21d.strftime("%d-%m-%Y"))
-btc_price_14d = get_coingecko_price_history(date14d.strftime("%d-%m-%Y"))
-btc_price_7d = get_coingecko_price_history(date7d.strftime("%d-%m-%Y"))
+print("VADER score for days 22-28: ", scores_21d)
+print("VADER score for days 15-21: ", scores_14d)
+print("VADER score for days 14-8: ", scores_7d)
 
-btc_price_22d = get_coingecko_price_history((date21d - datetime.timedelta(days=1)).strftime("%d-%m-%Y"))
-btc_price_15d = get_coingecko_price_history((date14d - datetime.timedelta(days=1)).strftime("%d-%m-%Y"))
-btc_price_8d = get_coingecko_price_history((date7d - datetime.timedelta(days=1)).strftime("%d-%m-%Y"))
+btc_price_21d = get_coingecko_price_history(date_21d.strftime("%d-%m-%Y"))
+btc_price_14d = get_coingecko_price_history(date_14d.strftime("%d-%m-%Y"))
+btc_price_7d = get_coingecko_price_history(date_7d.strftime("%d-%m-%Y"))
+
+btc_price_22d = get_coingecko_price_history((date_21d - datetime.timedelta(days=1)).strftime("%d-%m-%Y"))
+btc_price_15d = get_coingecko_price_history((date_14d - datetime.timedelta(days=1)).strftime("%d-%m-%Y"))
+btc_price_8d = get_coingecko_price_history((date_7d - datetime.timedelta(days=1)).strftime("%d-%m-%Y"))
 
 diff1 = btc_price_22d - btc_price_21d
 diff2 = btc_price_15d - btc_price_14d
 diff3 = btc_price_8d - btc_price_7d
 
-average_diff_ratio = ((diff1 / scores21d) + (diff2 / scores14d) + (diff3 / scores7d)) / 3
+average_diff_ratio = ((diff1 / scores_21d) + (diff2 / scores_14d) + (diff3 / scores_7d)) / 3
+print("Average three week price/score ratio: ", average_diff_ratio)
 
 comments = search_subs_reddit(q=query,
                    after=duration,
@@ -65,6 +70,7 @@ comments = search_subs_reddit(q=query,
                    subreddit=subs)
 
 scores = analyze_sentences(comments)
+print("VADER score for current week: ", scores)
 
 current_btc_price = get_coingecko_price()
 estimate_btc_price_24h = current_btc_price + (scores * average_diff_ratio)
